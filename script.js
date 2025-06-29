46497d7148d08737605a8650917ad7b80d3f8af7
relaya17 <139753005+relaya17@users.noreply.github.com>
2025-06-29 14:56:06 +0300

    ב

--- script.js
+++ script.js
@@ -125,6 +125,7 @@ function showPhoneNumber() {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 }
 document.addEventListener("DOMContentLoaded", function () {
 	const videoPlayer = document.getElementById("videoPlayer");
@@ -188,6 +189,9 @@ document.addEventListener("DOMContentLoaded", function () {
 		}
 	});
 });
+=======
+} 
+>>>>>>> parent of 3d03e57 (Merge branch 'main' of https://github.com/relaya17/actionsInTheDesert)
 =======
 } 
 >>>>>>> parent of 3d03e57 (Merge branch 'main' of https://github.com/relaya17/actionsInTheDesert)
